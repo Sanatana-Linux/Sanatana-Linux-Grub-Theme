7984733c81501985b4a8e5b67c3f04195879b865
Thomas Leon Highbaugh <me@thomasleonhighbaugh.me>
2025-01-30 05:56:47 -0800

    2025-01-30 05:56:47

--- flake.nix
+++ flake.nix
@@ -5,51 +5,47 @@
     nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in
-    with nixpkgs.lib;
-    {
-      nixosModule = { config, ... }:
-        let
-          cfg = config.boot.loader.grub.bhairava-grub-theme;
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in
+    with nixpkgs.lib; {
+      nixosModule = {config, ...}: let
+        cfg = config.boot.loader.grub.bhairava-grub-theme;
 
-          bhairava-grub-theme = pkgs.stdenv.mkDerivation {
-            name = "bhairava-grub-grub-theme";
-            src = ./.;
-            installPhase = ''
-              mkdir -p $out/grub/theme/icons
-              cp -rf backgrounds/bg.png $out/grub/theme/bg.png
-              cp -rf assets/assets/1080p/* $out/grub/theme/
-              cp -rf assets/logos/1080p/* $out/grub/theme/icons/ 
-              cp -rf common/* $out/grub/theme/
-              cp -rf config/theme-1080p.txt $out/grub/theme/theme.txt
-            '';
-          };
-        in
-        {
-          options = {
-            boot.loader.grub.bhairava-grub-theme = {
-              enable = mkOption {
-                type = types.bool;
-                default = false;
-                example = true;
-                description = ''
-                  Enable the Bhairava Grub Theme  
-                '';
-              };
+        bhairava-grub-theme = pkgs.stdenv.mkDerivation {
+          name = "bhairava-grub-grub-theme";
+          src = ./.;
+          installPhase = ''
+            mkdir -p $out/grub/theme/
+            cp -rf theme/* $out/grub/theme/
+          '';
+        };
+      in {
+        options = {
+          boot.loader.grub.bhairava-grub-theme = {
+            enable = mkOption {
+              type = types.bool;
+              default = false;
+              example = true;
+              description = ''
+                Enable the Bhairava Grub Theme
+              '';
             };
           };
+        };
 
-          config = mkIf cfg.enable (mkMerge [{
-            environment.systemPackages = [ bhairava-grub-theme ];
+        config = mkIf cfg.enable (mkMerge [
+          {
+            environment.systemPackages = [bhairava-grub-theme];
             boot.loader.grub = {
               theme = "${bhairava-grub-theme}/grub/theme";
             };
-          }]);
-        };
+          }
+        ]);
+      };
     };
 }
-
